938b1ee68e21339374e27470d77289079457adf6
David-Hari <d-hari@hotmail.com>
2015-08-03 21:28:46 +1000

    Better error messages.

--- app/src/main/java/david/htc_remote/Errors.java
+++ app/src/main/java/david/htc_remote/Errors.java
@@ -8,24 +8,24 @@ public class Errors {
     public static final SparseArray<String> map = new SparseArray<>();
 
     static {
-        map.put(CIRControl.ERR_NONE, "ERR_NONE");
-        map.put(CIRControl.ERR_UNKNOWN, "ERR_UNKNOWN");
-        map.put(CIRControl.ERR_CMD_FAILED, "ERR_CMD_FAILED");
-        map.put(CIRControl.ERR_HW_BUSY, "ERR_HW_BUSY");
-        map.put(CIRControl.ERR_IO_ERROR, "ERR_IO_ERROR");
-        map.put(CIRControl.ERR_CMD_DROPPED, "ERR_CMD_DROPPED");
-        map.put(CIRControl.ERR_TTY_SETUP_ERROR, "ERR_TTY_SETUP_ERROR");
-        map.put(CIRControl.ERR_TTY_WRITE_ERROR, "ERR_TTY_WRITE_ERROR");
-        map.put(CIRControl.ERR_TTY_READ_ERROR, "ERR_TTY_READ_ERROR");
-        map.put(CIRControl.ERR_INVALID_VALUE, "ERR_INVALID_VALUE");
-        map.put(CIRControl.ERR_LEARNING_TIMEOUT, "ERR_LEARNING_TIMEOUT");
-        map.put(CIRControl.ERR_CANCEL_FAIL, "ERR_CANCEL_FAIL");
-        map.put(CIRControl.ERR_CHECKSUM_ERROR, "ERR_CHECKSUM_ERROR");
-        map.put(CIRControl.ERR_OUT_OF_FREQ, "ERR_OUT_OF_FREQ");
-        map.put(CIRControl.ERR_CANCEL, "ERR_CANCEL");
-        map.put(CIRControl.ERR_PULSE_ERROR, "ERR_PULSE_ERROR");
-        map.put(CIRControl.ERR_PIPE_ERROR, "ERR_PIPE_ERROR");
-        map.put(CIRControl.ERR_OBJ_ERROR, "ERR_OBJ_ERROR");
+        map.put(CIRControl.ERR_NONE, "None");
+        map.put(CIRControl.ERR_UNKNOWN, "Unknown");
+        map.put(CIRControl.ERR_CMD_FAILED, "Command failed");
+        map.put(CIRControl.ERR_HW_BUSY, "Hardware busy");
+        map.put(CIRControl.ERR_IO_ERROR, "IO error");
+        map.put(CIRControl.ERR_CMD_DROPPED, "Command dropped");
+        map.put(CIRControl.ERR_TTY_SETUP_ERROR, "TTY setup error");
+        map.put(CIRControl.ERR_TTY_WRITE_ERROR, "TTY write error");
+        map.put(CIRControl.ERR_TTY_READ_ERROR, "TTY read error");
+        map.put(CIRControl.ERR_INVALID_VALUE, "Invalid value");
+        map.put(CIRControl.ERR_LEARNING_TIMEOUT, "Learning timeout");
+        map.put(CIRControl.ERR_CANCEL_FAIL, "Cancel fail");
+        map.put(CIRControl.ERR_CHECKSUM_ERROR, "Checksum error");
+        map.put(CIRControl.ERR_OUT_OF_FREQ, "Out of frequency");
+        map.put(CIRControl.ERR_CANCEL, "Cancel");
+        map.put(CIRControl.ERR_PULSE_ERROR, "Pulse error");
+        map.put(CIRControl.ERR_PIPE_ERROR, "Pipe error");
+        map.put(CIRControl.ERR_OBJ_ERROR, "Obj error");
     }
 
     static String stringFor(int code) {
